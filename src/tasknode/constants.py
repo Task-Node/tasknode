d49e74ff61892a6e7f453e555173eaf9ad3ee3e9
Daniel Newman <me@dtnewman.com>
2024-12-05 14:20:39 -0500

    update backend API url

--- src/tasknode/constants.py
+++ src/tasknode/constants.py
@@ -1,3 +1,2 @@
-# API_URL = "https://api-dev.tasknode.dev"  # Backend API URL
-API_URL = "http://127.0.0.1:5000"  # Local API URL
+API_URL = "https://api-dev.tasknode.dev"  # Backend API URL
 SERVICE_NAME = "tasknode-cli"
